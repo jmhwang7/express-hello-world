259bc3af877051d381b52307a98c94e993debcb8
Jennifer Hwang <jennifer@render.com>
2024-03-15 11:54:12 -0700

    change copy

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = 3004;
 
 app.get("/", (req, res) => res.type('html').send(html));
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const server = app.listen(port, () => console.log(`app listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
 server.headersTimeout = 120 * 1000;
